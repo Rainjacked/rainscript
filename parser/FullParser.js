4175f13078ce9807b8e48f3a10913dfd8129b60e
Rico Tiongson <thericotiongson@gmail.com>
2017-10-29 02:26:58 +0800

    Add comments remover.

--- parser/FullParser.js
+++ parser/FullParser.js
@@ -1,16 +1,17 @@
 import { LinkedList } from './util/LinkedList';
 import { Environment } from './util/Environment';
+import { CommentsRemover } from './util/CommentsRemover';
 import { FullParserTemplate } from './FullParserTemplate';
 
 export class FullParser {
   /**
    * Constructs this FullParser.
    */
-  constructor (wrapper) {
+  constructor () {
     // copy contents from template
     Object.assign(this, FullParserTemplate);
     // create buffer
-    this.replace('');
+    this.replace();
   }
 
   /**
@@ -18,13 +19,13 @@ export class FullParser {
    * @param {*} buffer the new buffer to replace with
    */
   replace (buffer) {
-    this.buffer = buffer || '';
+    this.buffer = '';
     this.index = 0;
     this.line = 1;
     this.errors = new LinkedList();
     this.warning = new LinkedList();
     this.environment = new Environment();
-    return this;
+    return buffer ? this.append(buffer) : this;
   }
 
   /**
@@ -32,7 +33,7 @@ export class FullParser {
    * @param {*} buffer the new information to append
    */
   append (buffer) {
-    this.buffer += buffer;
+    this.buffer += CommentsRemover.all(buffer);
     return this;
   }
 }
