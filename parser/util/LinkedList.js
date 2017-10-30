e75b6ffcafa235ef5cf8761791403f4f89573b7e
Rico Tiongson <thericotiongson@gmail.com>
2017-10-31 03:31:47 +0800

    [parser] Add LinkedList class

---
+++ parser/util/LinkedList.js
@@ -0,0 +1,23 @@
+/**
+ * A singly persistent linked list.
+ */
+export class LinkedList {
+  /**
+   * Constructs a new LinkedList.
+   * @param {*} head data associated with the first node in the linked list
+   * @param {*} tail a pointer to thenext node in the linked list
+   */
+  constructor (head, tail) {
+    let size = tail instanceof LinkedList ? tail.size() + 1 : 0;
+    this.head = (h) => h === undefined ? head : (head = h);
+    this.tail = () => tail;
+    this.size = () => size;
+  }
+  /**
+   * Creates a new LinkedList extended with data as new head.
+   * @param {*} data the new head of the extended linked list
+   */
+  push (data) {
+    return new LinkedList(data, this);
+  }
+}
