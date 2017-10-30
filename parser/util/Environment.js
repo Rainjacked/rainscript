9de2d1a2df01398619b443a3489844d63dc8c024
Rico Tiongson <thericotiongson@gmail.com>
2017-10-31 03:37:15 +0800

    [parser] Add Environment-type linked list

---
+++ parser/util/Environment.js
@@ -0,0 +1,30 @@
+import { LinkedList } from './LinkedList';
+
+/**
+ * A special key-value pair linked list that can lookup values of a certain key.
+ */
+export class Environment extends LinkedList {
+  /**
+   * Creates a new environment with an associated key-value pair.
+   * @param {*} key   the searchable identifier that maps to the value
+   * @param {*} value the value associated with the key
+   * @return a new Environment with the appended key-value pair
+   */
+  push (key, value) {
+    return new Environment([key, value], this);
+  }
+  /**
+   * Searches for the value of the nearest key in this environment.
+   * @param {*} key the searchable identifier that maps to the value
+   * @return the value of the nearest key, or undefined if not found
+   */
+  lookup (key) {
+    if (this.head() === undefined) {
+      return undefined;
+    }
+    if (this.head()[0] === key) {
+      return this.head()[1];
+    }
+    return this.tail().lookup(key);
+  }
+}
