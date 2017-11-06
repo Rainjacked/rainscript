510411e794fc13c1261aca42ac52178b03a8490d
Rico Tiongson <thericotiongson@gmail.com>
2017-11-06 18:53:58 +0800

    [parser] [fix] Adopt Chai should syntax for tests

--- tests/parser/util/Environment.js
+++ tests/parser/util/Environment.js
@@ -1,55 +1,61 @@
 import { Environment } from '../../../parser/util/Environment';
 import { LinkedList } from '../../../parser/util/LinkedList';
-const assert = require('assert');
+import { _ } from 'underscore';
+require('chai').should();
 
 describe('parser', () => {
   describe('util', () => {
     describe('Environment', () => {
       it('should be an instance of LinkedList', () => {
-        let list = new Environment();
-        assert(list instanceof LinkedList);
+        new Environment().should.be.an.instanceof(LinkedList);
       });
 
       it('should throw an error when trying to access the head', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
+        let headGetter = () => {
           return list.head;
-        }, Error);
+        };
+        headGetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the head', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
-          list.head = ['one', 1];
-        }, Error);
+        let headSetter = () => {
+          list.head = 'not one';
+        };
+        headSetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the key', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
+        let keySetter = () => {
           list.key = 'one';
-        }, Error);
+        };
+        keySetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the value', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
+        let valueSetter = () => {
           list.value = 1;
-        }, Error);
+        };
+        valueSetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the tail', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
-          list.tail = [];
-        }, Error);
+        let tailSetter = () => {
+          list.tail = 2;
+        };
+        tailSetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the size', () => {
         let list = new Environment().push('one', 1);
-        assert.throws(() => {
+        let sizeSetter = () => {
           list.size = 2;
-        }, Error);
+        };
+        sizeSetter.should.throw(Error);
       });
 
       it('should be able to lookup values', () => {
@@ -59,33 +65,32 @@ describe('parser', () => {
         list = list.push('three', 3);
         list = list.push('four', 4);
         list = list.push('five', 5);
-        assert.strictEqual(list.lookup('one'), 1);
-        assert.strictEqual(list.lookup('two'), 2);
-        assert.strictEqual(list.lookup('three'), 3);
-        assert.strictEqual(list.lookup('four'), 4);
-        assert.strictEqual(list.lookup('five'), 5);
+        list.lookup('one').should.equal(1);
+        list.lookup('two').should.equal(2);
+        list.lookup('three').should.equal(3);
+        list.lookup('four').should.equal(4);
+        list.lookup('five').should.equal(5);
       });
 
       it('should return null if key not found', () => {
         let list = new Environment();
         list = list.push('one', 1);
-        assert.strictEqual(list.lookup('two'), undefined, 'key not found ' +
-          'should return undefined');
+        _.isUndefined(list.lookup('two')).should.be.true;
+        _.isUndefined(list.lookup('two')).should.be.true;
         list = list.push('two', 2);
-        assert.strictEqual(list.lookup('two'), 2);
+        list.lookup('two').should.equal(2);
       });
 
       it('should shadow lookups', () => {
         let zero = new Environment();
         let one = zero.push('one', 1);
-        assert.strictEqual(one.lookup('one'), 1, 'one should be 1');
+        one.lookup('one').should.equal(1);
         let two = one.push('two', 2);
         let three = two.push('three', 3);
         let oneAgain = three.push('one', -1);
         let four = oneAgain.push('four', 4);
-        assert.strictEqual(three.lookup('one'), 1, 'three should be 1');
-        assert.strictEqual(four.lookup('one'), -1,
-          'four should be shadowed with -1');
+        three.lookup('one').should.equal(1);
+        four.lookup('one').should.equal(-1, 'four should be shadowed');
       });
     });
   });
--- tests/parser/util/LinkedList.js
+++ tests/parser/util/LinkedList.js
@@ -1,55 +1,59 @@
 import { LinkedList } from '../../../parser/util/LinkedList';
-const assert = require('assert');
+import { _ } from 'underscore';
+require('chai').should();
 
 describe('parser', () => {
   describe('util', () => {
     describe('LinkedList', () => {
       it('should have size 0 when empty', () => {
         let list = new LinkedList();
-        assert.ok(list);
-        assert.strictEqual(list.size, 0);
+        list.should.exist;
+        list.size.should.equal(0);
       });
 
       it('should have undefined fields on empty construction', () => {
         let list = new LinkedList();
-        assert.strictEqual(list.head, undefined);
-        assert.strictEqual(list.tail, undefined);
+        _.isUndefined(list.head).should.be.true;
+        _.isUndefined(list.tail).should.be.true;
       });
 
       it('should throw an error when trying to set the head', () => {
         let list = new LinkedList().push('one', 1);
-        assert.throws(() => {
-          list.head = ['one', 1];
-        }, Error);
+        let headSetter = () => {
+          list.head = 'not one';
+        };
+        headSetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the tail', () => {
         let list = new LinkedList().push('one', 1);
-        assert.throws(() => {
-          list.tail = [];
-        }, Error);
+        let tailSetter = () => {
+          list.tail = 2;
+        };
+        tailSetter.should.throw(Error);
       });
 
       it('should throw an error when trying to set the size', () => {
         let list = new LinkedList().push('one', 1);
-        assert.throws(() => {
+        let sizeSetter = () => {
           list.size = 2;
-        }, Error);
+        };
+        sizeSetter.should.throw(Error);
       });
 
       it('should have correct links', () => {
         let list1 = new LinkedList();
         let list2 = list1.push('hello');
         let list3 = list2.push('world');
-        assert.strictEqual(list1.head, undefined, 'list 1 head undefiend');
-        assert.strictEqual(list2.head, 'hello', 'list 2 head hello');
-        assert.strictEqual(list3.head, 'world', 'list 3 head world');
-        assert.strictEqual(list1.tail, undefined, 'list 1 tail undefined');
-        assert.strictEqual(list2.tail, list1, 'list 2 tail list 1');
-        assert.strictEqual(list3.tail, list2, 'list 3 tail list 2');
-        assert.strictEqual(list1.size, 0, 'list 1 size 0');
-        assert.strictEqual(list2.size, 1, 'list 2 size 1');
-        assert.strictEqual(list3.size, 2, 'list 3 size 2');
+        _.isUndefined(list1.head).should.be.true;
+        list2.head.should.equal('hello');
+        list3.head.should.equal('world');
+        _.isUndefined(list1.tail).should.be.true;
+        list2.tail.should.equal(list1);
+        list3.tail.should.equal(list2);
+        list1.size.should.equal(0);
+        list2.size.should.equal(1);
+        list3.size.should.equal(2);
       });
 
       it('should be persistent', () => {
@@ -57,13 +61,11 @@ describe('parser', () => {
         let list2 = list1.push('common');
         let list3 = list2.push('hello');
         let list4 = list2.push('world');
-        assert.strictEqual(list2.head, 'common', 'list 2 head common');
-        assert.strictEqual(list3.head, 'hello', 'list 3 head hello');
-        assert.strictEqual(list3.tail.head, 'common',
-          'list 3 parent common');
-        assert.strictEqual(list4.head, 'world', 'list 4 world');
-        assert.strictEqual(list4.tail.head, 'common',
-          'list 4 parent common');
+        list2.head.should.equal('common');
+        list3.head.should.equal('hello');
+        list3.tail.head.should.equal('common');
+        list4.head.should.equal('world');
+        list4.tail.head.should.equal('common');
       });
     });
   });
