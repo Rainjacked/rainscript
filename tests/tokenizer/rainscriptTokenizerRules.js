b6f7473805821f1e854417906d9a2faf94aed97a
Rico Tiongson <thericotiongson@gmail.com>
2017-11-09 14:58:27 +0800

    [tokenizer] Add rule VAR_SUFFIX

--- tests/tokenizer/rainscriptTokenizerRules.js
+++ tests/tokenizer/rainscriptTokenizerRules.js
@@ -3,14 +3,29 @@ require('chai').should();
 
 describe('tokenizer', () => {
   describe('rainscriptTokenizerRules', () => {
-    it('should tokenize EVENT', () => {
-      let tokenizer = rainscriptTokenizer();
-      let tokens = tokenizer.tokenize('[[ I AM AN EVENT ]]');
-      tokens.should.be.an('array').with.lengthOf(1);
-      tokens[0].should.include({
-        type: 'EVENT',
-        lexeme: '[[ I AM AN EVENT ]]'
+    describe('EVENT', () => {
+      it('should tokenize EVENT', () => {
+        let tokenizer = rainscriptTokenizer();
+        let tokens = tokenizer.tokenize('[[ I AM AN EVENT ]]');
+        tokens.should.be.an('array').with.lengthOf(1);
+        tokens[0].should.include({
+          type: 'EVENT',
+          lexeme: '[[ I AM AN EVENT ]]'
+        });
       });
+    });
+
+    describe('VAR_SUFFIX', () => {
+      it('should tokenize VAR_SUFFIX', () => {
+        let tokenizer = rainscriptTokenizer();
+        let tokens = tokenizer.tokenize('p055ible_var_name');
+        tokens.should.be.an('array').with.lengthOf(1);
+        tokens[0].should.include({
+          type: 'VAR_SUFFIX',
+          lexeme: 'p055ible_var_name'
+        });
+      });
+      // TODO: add more comprehensive tests for VAR_SUFFIX
     });
   });
 });
--- tokenizer/rainscriptTokenizerRules.js
+++ tokenizer/rainscriptTokenizerRules.js
@@ -10,5 +10,6 @@
  */
 export const rainscriptTokenizerRules = [
   { pattern: /\[\[[^\]]*\]\]/, type: 'EVENT' },
+  { pattern: /[a-zA-Z_][0-9a-zA-Z_]*/, type: 'VAR_SUFFIX' },
   { pattern: /\s+/, type: 'WHITESPACE' }
 ];
