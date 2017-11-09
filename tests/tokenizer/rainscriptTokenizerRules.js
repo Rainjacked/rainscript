8ed386e694d6b069905a70667476818958e0d348
Rico Tiongson <thericotiongson@gmail.com>
2017-11-09 14:20:33 +0800

    [tokenizer] Add rule COMMENT
    Parses C-style inline comments and block-comments.

--- tests/tokenizer/rainscriptTokenizerRules.js
+++ tests/tokenizer/rainscriptTokenizerRules.js
@@ -3,6 +3,32 @@ require('chai').should();
 
 describe('tokenizer', () => {
   describe('rainscriptTokenizerRules', () => {
+    describe('COMMENT', () => {
+      it('should tokenize inline COMMENT', () => {
+        let tokenizer = rainscriptTokenizer();
+        let tokens = tokenizer.tokenize('// inline comment\n// another');
+        tokens.should.be.an('array').with.lengthOf(3);
+        tokens[0].should.include({
+          type: 'COMMENT',
+          lexeme: '// inline comment'
+        });
+        tokens[2].should.include({
+          type: 'COMMENT',
+          lexeme: '// another'
+        });
+      });
+
+      it('should tokenize block COMMENT', () => {
+        let tokenizer = rainscriptTokenizer();
+        let tokens = tokenizer.tokenize('/* block \n comment */');
+        tokens.should.be.an('array').with.lengthOf(1);
+        tokens[0].should.include({
+          type: 'COMMENT',
+          lexeme: '/* block \n comment */'
+        });
+      });
+    });
+
     describe('EVENT', () => {
       it('should tokenize EVENT', () => {
         let tokenizer = rainscriptTokenizer();
--- tokenizer/rainscriptTokenizerRules.js
+++ tokenizer/rainscriptTokenizerRules.js
@@ -9,6 +9,8 @@
  *   }[]
  */
 export const rainscriptTokenizerRules = [
+  // parse c-style inline and block comments
+  { pattern: /\/\*[^*]*\*+(?:[^/*][^*]*\*+)*\/|\/\/[^\n]*/, type: 'COMMENT' },
   { pattern: /\[\[[^\]]*\]\]/, type: 'EVENT' },
   { pattern: /[a-zA-Z_][0-9a-zA-Z_]*/, type: 'VAR_SUFFIX' },
   { pattern: /\s+/, type: 'WHITESPACE' }
