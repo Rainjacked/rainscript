9766a8cbd235318335e208c837841c0bc38d7517
Rico Tiongson <thericotiongson@gmail.com>
2017-11-09 15:23:24 +0800

    [tokenizer] Add rule FLOAT
    Recognize real numbers with decimal points, possibly in scientific notation

--- tests/tokenizer/rainscriptTokenizerRules.js
+++ tests/tokenizer/rainscriptTokenizerRules.js
@@ -123,6 +123,38 @@ describe('tokenizer', () => {
       });
     });
 
+    describe('FLOAT', () => {
+      function recognize (input, message) {
+        let tokenizer = rainscriptTokenizer();
+        let tokens = tokenizer.tokenize(input);
+        tokens.should.be.an('array').with.lengthOf(1);
+        tokens[0].should.include({
+          type: 'FLOAT',
+          lexeme: input
+        }, message);
+      }
+
+      it('should tokenize numbers with decimal points as FLOAT', () => {
+        recognize('12.213540', 'decimal');
+        recognize('-687.05132', 'negative decimal');
+        recognize('+24.4454321', 'positive decimal');
+      });
+
+      it('should tokenize lowercase scientific notation as FLOAT', () => {
+        recognize('1e12', 'scientific');
+        recognize('-3e-6', 'negative scientific');
+        recognize('+7e+19', 'positive scientific');
+        recognize('123.817e19', 'decimal scientific');
+      });
+
+      it('should tokenize uppercase scientific notation as FLOAT', () => {
+        recognize('1E12', 'scientific');
+        recognize('-3E-6', 'negative scientific');
+        recognize('+7E+19', 'positive scientific');
+        recognize('123.817E19', 'decimal scientific');
+      });
+    });
+
     describe('COMMENT', () => {
       it('should tokenize inline COMMENT', () => {
         let tokenizer = rainscriptTokenizer();
--- tokenizer/rainscriptTokenizerRules.js
+++ tokenizer/rainscriptTokenizerRules.js
@@ -9,11 +9,14 @@
  *   }[]
  */
 export const rainscriptTokenizerRules = [
-  // parse quoted string literals
+  // quoted string literals
   { pattern: /'(\\.|[^\\'])*'/, type: 'STR_1' },
   { pattern: /"(\\.|[^\\"])*"/, type: 'STR_2' },
-  // parse c-style inline and block comments
+  // floating-point literal
+  { pattern: /[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?/, type: 'FLOAT' },
+  // c-style inline and block comments
   { pattern: /\/\*[^*]*\*+(?:[^/*][^*]*\*+)*\/|\/\/[^\n]*/, type: 'COMMENT' },
+  // event name
   { pattern: /\[\[[^\]]*\]\]/, type: 'EVENT' },
   { pattern: /[a-zA-Z_][0-9a-zA-Z_]*/, type: 'VAR_SUFFIX' },
   { pattern: /\s+/, type: 'WHITESPACE' }
